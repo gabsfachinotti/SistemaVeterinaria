f01f526c36e59c22f1215548195f6e93ee99947a
Gabriel Sosa Fachinotti <gabsfachinotti@live.com>
2018-03-06 20:35:04 -0300

    ultimos cambios

--- SistemaVeterinaria/App_Start/BundleConfig.cs
+++ SistemaVeterinaria/App_Start/BundleConfig.cs
@@ -5,39 +5,45 @@ namespace SistemaVeterinaria
 {
     public class BundleConfig
     {
-        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-2.1.2.js",
-                        "~/Scripts/bootbox.js",
-                        "~/Scripts/bootstrap.js",
-                        "~/Scripts/respond.js",
-                        "~/Scripts/select2.js",
-                        "~/Scripts/toastr.js",
-                        "~/Scripts/DataTables/dataTables.bootstrap.js",
-                        "~/Scripts/DataTables/jquery.dataTables.js",
-                        "~/Scripts/jquery.steps.min.js"));
+            BundleTable.EnableOptimizations = true;
+            ScriptBundle scriptBundle = new ScriptBundle("~/js");
+            string[] scriptArray =
+            {
+                "~/Scripts/jquery-2.1.2.js",
+                "~/Scripts/bootbox.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js",
+                "~/Scripts/select2.js",
+                "~/Scripts/toastr.js",
+                "~/Scripts/DataTables/dataTables.bootstrap.js",
+                "~/Scripts/DataTables/jquery.dataTables.js",
+                "~/Scripts/jquery.steps.min.js"
+        };
+            scriptBundle.Include(scriptArray);
+            scriptBundle.IncludeDirectory("~/Scripts/", "*.js");
+            bundles.Add(scriptBundle);
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-2.6.2.js"));
+                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/css/select2.css",
-                      "~/Content/DataTables/css/jquery.dataTables.css",
-                      "~/Content/toastr.css",
-                      "~/Content/jquery.steps.css"));
+
+            bundles.Add(new StyleBundle("~/Styles/css").Include(
+                                       "~/Content/bootstrap.css",
+                                       "~/Content/site.css",
+                                       "~/Content/css/select2.css",
+                                       "~/Content/DataTables/css/jquery.dataTables.css",
+                                       "~/Content/toastr.css",
+                                       "~/Content/jquery.steps.css"
+                     ));
         }
     }
 }
