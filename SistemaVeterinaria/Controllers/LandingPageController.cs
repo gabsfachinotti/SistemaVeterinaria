858b18228fbbd57a7164d215530ac9588abb6ed0
Gabriel Sosa Fachinotti <gabsfachinotti@live.com>
2018-03-08 22:13:34 -0300

    Portada

---
+++ SistemaVeterinaria/Controllers/LandingPageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistemaVeterinaria.Context;
+using SistemaVeterinaria.Models;
+
+namespace SistemaVeterinaria.Controllers
+{
+    public class LandingPageController : Controller
+    {
+        private VeterinaryContext db = new VeterinaryContext();
+        // GET: LandingPage
+        public ActionResult Index()
+        {
+            ViewBag.Vaccines = db.Vaccines.Count(v => v.VaccineDate == DateTime.Today & v.VaccineNumber != 1);
+            ViewBag.Surgeries = db.Surgeries.Count(s => s.SurgeryDate == DateTime.Today);
+            ViewBag.Showers = db.Showers.Count(sh => sh.ShowerDate == DateTime.Today);
+
+            return View();
+        }
+    }
+}
